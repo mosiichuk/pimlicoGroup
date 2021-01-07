94dd20d105dd4db93dcde252e6200e0ccd3a27a2
Yevhen Mosiichuk <zhenya9801@gmail.com>
2021-01-07 19:38:03 +0200

    Small fixes #2

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap";
 import "css-reset-and-normalize";
-import 'swiper/swiper-bundle.min.css';
 
 import './js/index';
 import './styles/style.sass';
